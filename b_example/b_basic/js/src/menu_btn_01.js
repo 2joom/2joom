5d175d92b3a7bc45a116faf8b1a72a35f7e95957
sun <vividkim4228@gmail.com>
2018-11-27 16:05:07 +0900

    181127

---
+++ b_example/b_basic/js/src/menu_btn_01.js
@@ -0,0 +1,40 @@
+/* menu_btn_01.js */
+/*
+// 함수 표현식(익명함수(이름이없는 함수)를 변수에 대입처리)
+var Fn = function(e){
+  return e+5;
+};
+var a = Fn(10);
+console.log(a);
+
+// 함수 선언식(함수자체에 이름을 생성)
+function Fn2(e) {
+  return e * 2;
+}
+var b = Fn2(5);
+console.log(b);
+
+//테스트
+var tt = ("snan" + "momo")+'jiyoun';
+console.log(tt);
+
+//즉시 실행함수
+(function(e){
+  var j =  e *2 +3;
+  console.log(j);
+  return j;
+})(3);
+*/
+
+//-------------------------------------------
+(function($){
+
+
+})(jQuery);
+
+// -------------------------------------------
+( function($){ }(jQuery) );
+
+
+// -------------------------------------------
+$(document).ready(function(){ });//브라우저(html)가 준비가 되면 실행하라
