cba624bf753b884c30fffa43eb81c47fc39e7fc6
2joom <dlwnal9295@gmail.com>
2018-12-15 22:21:15 +0900

    정리

--- b_example/c_normal/js/src/gcass/c.js
+++ b_example/c_normal/js/src/gcass/c.js
@@ -1,17 +1,17 @@
 // main_01.js
 (function ($){
-  //console.log('!!!!!!');
+  console.log('!!!!!!');
 
-  var aUrl = '../commonBox/'
+  //var aUrl = '../commonBox/'
   var homeBox = $('#homeBox');
   var headBox = $('#headBox');
   var footBox = $('#footBox');
   // load() : 외부에 존재하는 문서를 불러와서 담는 기능
   // 반드시 서버로만 구동되어야한다!!
   // html 문서의 경우 기준 상대경로는 삽입하려고하는 문서(html)를 기준으로 한다. 
-  homeBox.load( aUrl + '/homeBox.html');
-  headBox.load( aUrl + '/headBox.html');
-  footBox.load( aUrl + '/footBox.html');
+  homeBox.load( '../commonBox/homeBox.html');
+  headBox.load( '../commonBox/headBox.html');
+  footBox.load( '../commonBox/footBox.html');
 
 
 })(jQuery);
